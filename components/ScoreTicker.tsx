b7a58132e06ebae24ad14132645f055dd6f7d6ec
yunji0387 <yunji0387@gmail.com>
2024-03-25 15:46:20 -0500

    Increase font size for score in ScoreTicker component

--- components/ScoreTicker.tsx
+++ components/ScoreTicker.tsx
@@ -184,7 +184,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   score: {
-    fontSize: hp("2%"),
+    fontSize: hp("2.5%"),
     fontWeight: "bold",
     color: COLORS.white,
   },
